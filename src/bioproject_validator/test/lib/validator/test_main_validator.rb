5735d59b5a139c96245dd8ba92b8e5e78d2494d7
okbp <yokookbio@gmail.com>
2017-05-24 03:16:36 +0900

    Added some bioproject validation of empty check

--- src/bioproject_validator/test/lib/validator/test_main_validator.rb
+++ src/bioproject_validator/test/lib/validator/test_main_validator.rb
@@ -62,6 +62,25 @@ class TestMainValidator < Minitest::Test
   end
 
 #### 各validationメソッドのユニットテスト ####
+  # rule:5
+  def test_identical_project_title_and_description
+    #ok case
+    project_set = get_project_set_node("../../data/5_identical_project_title_and_description_ok.xml")
+    ret = exec_validator("identical_project_title_and_description", "5", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not use Description element
+    project_set = get_project_set_node("../../data/5_identical_project_title_and_description_ok2.xml")
+    ret = exec_validator("identical_project_title_and_description", "5", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/5_identical_project_title_and_description_ng.xml")
+    ret = exec_validator("identical_project_title_and_description", "5", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
   # rule:6
   def test_short_project_description
     #ok case
@@ -89,7 +108,7 @@ class TestMainValidator < Minitest::Test
     assert_equal true, ret[:result]
     assert_equal 0, ret[:error_list].size
     #not use Other element
-    project_set = get_project_set_node("../../data/7_empty_description_for_other_relevance_ok_2.xml")
+    project_set = get_project_set_node("../../data/7_empty_description_for_other_relevance_ok2.xml")
     ret = exec_validator("empty_description_for_other_relevance", "7", "project name" , project_set.first, 1)
     assert_equal true, ret[:result]
     assert_equal 0, ret[:error_list].size
@@ -120,6 +139,120 @@ class TestMainValidator < Minitest::Test
     #ng case
     project_set = get_project_set_node("../../data/8_empty_description_for_other_subtype_ng.xml")
     ret = exec_validator("empty_description_for_other_subtype", "8", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:9
+  def test_empty_target_description_for_other_sample_scope
+    #ok case
+    project_set = get_project_set_node("../../data/9_empty_target_description_for_other_sample_scope_ok.xml")
+    ret = exec_validator("empty_target_description_for_other_sample_scope", "9", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/9_empty_target_description_for_other_sample_scope_ok2.xml")
+    ret = exec_validator("empty_target_description_for_other_sample_scope", "9", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/9_empty_target_description_for_other_sample_scope_ng.xml")
+    ret = exec_validator("empty_target_description_for_other_sample_scope", "9", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:10
+  def test_empty_target_description_for_other_material
+    #ok case
+    project_set = get_project_set_node("../../data/10_empty_target_description_for_other_material_ok.xml")
+    ret = exec_validator("empty_target_description_for_other_material", "10", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/10_empty_target_description_for_other_material_ok2.xml")
+    ret = exec_validator("empty_target_description_for_other_material", "10", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/10_empty_target_description_for_other_material_ng.xml")
+    ret = exec_validator("empty_target_description_for_other_material", "10", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:11
+  def test_empty_target_description_for_other_capture
+    #ok case
+    project_set = get_project_set_node("../../data/11_empty_target_description_for_other_capture_ok.xml")
+    ret = exec_validator("empty_target_description_for_other_capture", "11", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/11_empty_target_description_for_other_capture_ok2.xml")
+    ret = exec_validator("empty_target_description_for_other_capture", "11", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/11_empty_target_description_for_other_capture_ng.xml")
+    ret = exec_validator("empty_target_description_for_other_capture", "11", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:12
+  def test_empty_method_description_for_other_method_type
+    #ok case
+    project_set = get_project_set_node("../../data/12_empty_method_description_for_other_method_type_ok.xml")
+    ret = exec_validator("empty_method_description_for_other_method_type", "12", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/12_empty_method_description_for_other_method_type_ok2.xml")
+    ret = exec_validator("empty_method_description_for_other_method_type", "12", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/12_empty_method_description_for_other_method_type_ng.xml")
+    ret = exec_validator("empty_method_description_for_other_method_type", "12", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:13
+  def test_empty_data_description_for_other_data_type
+    #ok case
+    project_set = get_project_set_node("../../data/13_empty_data_description_for_other_data_type_ok.xml")
+    ret = exec_validator("empty_data_description_for_other_data_type", "13", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/13_empty_data_description_for_other_data_type_ok2.xml")
+    ret = exec_validator("empty_data_description_for_other_data_type", "13", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/13_empty_data_description_for_other_data_type_ng.xml")
+    ret = exec_validator("empty_data_description_for_other_data_type", "13", "project name" , project_set.first, 1)
+    assert_equal false, ret[:result]
+    assert_equal 1, ret[:error_list].size
+  end
+
+  # rule:15
+  def test_empty_publication_reference
+    #ok case
+    project_set = get_project_set_node("../../data/15_empty_publication_reference_ok.xml")
+    ret = exec_validator("empty_publication_reference", "15", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #not eOther attribute
+    project_set = get_project_set_node("../../data/15_empty_publication_reference_ok2.xml")
+    ret = exec_validator("empty_publication_reference", "15", "project name" , project_set.first, 1)
+    assert_equal true, ret[:result]
+    assert_equal 0, ret[:error_list].size
+    #ng case
+    project_set = get_project_set_node("../../data/15_empty_publication_reference_ng.xml")
+    ret = exec_validator("empty_publication_reference", "15", "project name" , project_set.first, 1)
     assert_equal false, ret[:result]
     assert_equal 1, ret[:error_list].size
   end
