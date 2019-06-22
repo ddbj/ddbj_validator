64002c4f89f48936ee7078dcdcb86c5c5a650cbd
okbp <yokookbio@gmail.com>
2019-06-22 17:11:37 +0900

    Accept data post

--- src/app/application.rb
+++ src/app/application.rb
@@ -46,6 +46,11 @@ module DDBJValidator
       send_file File.join(settings.public_folder, 'apispec/index.html')
     end
 
+    get '/api/client/index_data_post' do
+      content_type 'text/html; charset=utf-8'
+      erb :index_data_post
+    end
+
     get '/api/client/index' do
       content_type 'text/html; charset=utf-8'
       erb :index
@@ -336,14 +341,22 @@ module DDBJValidator
         file_combination
       end
 
-      #file保存を保存し、ファイルパスを返す
+      #fileを保存し、ファイルパスを返す
       def save_file (output_dir, validator_type, params)
         save_dir = "#{output_dir}/#{validator_type}"
         FileUtils.mkdir_p(save_dir)
-        save_path = save_dir + "/" + params[validator_type.to_sym][:filename]
-        File.open(save_path, 'wb') do |f|
-          f.write params[validator_type.to_sym][:tempfile].read
+        if params[validator_type.to_sym].is_a?(String) #fileではなくデータで送られた場合
+          save_path = save_dir + "/#{validator_type}" #ファイル名はデータの種類名("biosample"等)
+          File.open(save_path, 'wb') do |f|
+            f.write params[validator_type.to_sym]
+          end
+        else
+          save_path = save_dir + "/" + params[validator_type.to_sym][:filename]
+          File.open(save_path, 'wb') do |f|
+            f.write params[validator_type.to_sym][:tempfile].read
+          end
         end
+
         save_path
       end
 
