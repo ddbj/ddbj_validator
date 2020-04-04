de5839d0ed4e77fe9a38e25c399d5d1625f73b66
okbp <yokookbio@gmail.com>
2020-04-04 15:48:40 +0900

    Update deploy_tools for clustering

---
+++ deploy_tools/update_validator_dbfile_it048_to_it047.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -l
+
+scp /data1/ddbj/DDBJValidator/data_updater/dbfile/virtuoso.db it047:/data1/ddbj/DDBJValidator/data_updater/dbfile/virtuoso.db
---
+++ deploy_tools/update_validator_dbfile_production1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#set -e
+
+BASE_DIR=/data1/ddbj/DDBJValidator/validator_production1
+VIRTUOSO_CONTAINER_NAME=ddbj_validator_virtuoso_production1
+VIRT_PORT=18801
+VIRT_HOME=$BASE_DIR/shared/data/virtuoso/
+
+ROOT_DIR=$(cd ./$(dirname $0); pwd)
+source $ROOT_DIR/update_validator_dbfile
