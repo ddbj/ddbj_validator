599664235dc52b1740f9338574811c701380e5ce
okbp <yokookbio@gmail.com>
2020-04-05 01:25:00 +0900

    Fixed bug. update dbfile path

--- deploy_tools/update_validator_dbfile_production1.sh
+++ deploy_tools/update_validator_dbfile_production1.sh
@@ -6,5 +6,5 @@ VIRTUOSO_CONTAINER_NAME=ddbj_validator_virtuoso_production1
 VIRT_PORT=18801
 VIRT_HOME=$BASE_DIR/shared/data/virtuoso/
 
-ROOT_DIR=$(cd ./$(dirname $0); pwd)
+ROOT_DIR=$(cd $(dirname $0); pwd)
 source $ROOT_DIR/update_validator_dbfile
--- deploy_tools/update_validator_dbfile_production2.sh
+++ deploy_tools/update_validator_dbfile_production2.sh
@@ -6,5 +6,5 @@ VIRTUOSO_CONTAINER_NAME=ddbj_validator_virtuoso_production2
 VIRT_PORT=18811
 VIRT_HOME=$BASE_DIR/shared/data/virtuoso/
 
-ROOT_DIR=$(cd ./$(dirname $0); pwd)
+ROOT_DIR=$(cd $(dirname $0); pwd)
 source $ROOT_DIR/update_validator_dbfile
