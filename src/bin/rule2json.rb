a544d107d8e9be4fecf4ce9f303fbbf99542a4f6
okbp <yokookbio@gmail.com>
2020-04-14 12:56:14 +0900

    Added new biosample rule

--- src/bin/rule2json.rb
+++ src/bin/rule2json.rb
@@ -1,5 +1,5 @@
 # rule2json.rb
-# 
+#
 # Converts the rule configuration from google spreadsheet to json file.
 # output file is :  ./rule_config_(validator_name).json
 #
@@ -8,85 +8,63 @@ require 'csv'
 require 'json'
 
 class CreateRuleJson
+  # rule_file on googledocs
+  # biosample: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=0&format=csv"
+  # bioproject: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=423271243&format=csv"
+  # dra: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=2114094606&format=csv"
 
-  def initialize
-    @rule_conf = {
-      "biosample" => {
-        csv_file: "./rule_biosample.csv",
-        rule_url: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=0&format=csv"
-      },
-      "bioproject" => {
-        csv_file: "./rule_bioproject.csv",
-        rule_url: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=423271243&format=csv"
-      },
-      "dra" => {
-        csv_file: "./rule_dra.csv",
-        rule_url: "https://docs.google.com/spreadsheets/d/15pENGHA9hkl6QIueFb44fhQfQMThRB2tbvSE6hItHEU/export?gid=2114094606&format=csv"
-      }
-    }
-  end
-
-  def create_rule_file (type)
-    conf = @rule_conf[type]
-    unless conf.nil?
-      csv_file = conf[:csv_file]
-      rule_url = conf[:rule_url]
-      # download spreadsheets as csv
-      system(%Q[curl -o #{csv_file} "#{rule_url}"])
-
-      # parse csv
-      csv_data = CSV.read("#{csv_file}", headers: true)
-      rule_config = []
-      csv_data.each do |row|
-        next unless row["rule_status"].nil?
-        next if row["name"].nil?
-        hash = {}
-        hash[:rule_class] = row["rule_class"]
-        hash[:code] = row["rule_id"]
-        hash[:level] = row["level"]
-        if row["internal_ignore"].nil? || row["internal_ignore"].strip == ""
-          hash[:internal_ignore] = false
-        else
-          hash[:internal_ignore] = true
-        end
-        hash[:name] = row["name"]
-        hash[:method] = row["name"].gsub(" ", "_").gsub(">", "_").gsub("-", "_").gsub("/", "_").downcase
-        hash[:message] = row["message"]
-        #hash[:object] = "BioSample, BioProject and Submission AND Run".split(/and|,/i).map {|obj| obj.chomp.strip }
-        hash[:object] = row["object"].split(/and|,/i).map {|obj| obj.chomp.strip }
-        unless row["reference"].nil? || row["reference"].strip == ""
-          hash[:reference] = row["reference"]
-        end
-        rule_config.push(hash)
+  def create_rule_file (type, csv_file)
+    unless File.exist?(csv_file)
+      puts "ERROR: not found rule csv file: #{csv_file}"
+      exit(1)
+    end
+    # parse csv
+    csv_data = CSV.read("#{csv_file}", headers: true)
+    rule_config = []
+    csv_data.each do |row|
+      next unless row["rule_status"].nil?
+      next if row["name"].nil?
+      hash = {}
+      hash[:rule_class] = row["rule_class"]
+      hash[:code] = row["rule_id"]
+      hash[:level] = row["level"]
+      if row["internal_ignore"].nil? || row["internal_ignore"].strip == ""
+        hash[:internal_ignore] = false
+      else
+        hash[:internal_ignore] = true
       end
-
-      #output
-      output_dir = File.absolute_path(File.dirname(__FILE__))
-
-      # separate the rule config file to each validator
-      rule_classes = rule_config.group_by {|rule| rule[:rule_class]}
-      rule_classes.each do |rule_class, rule_list|
-        output_file = "#{output_dir}/rule_config_#{rule_class.downcase}.json"
-        rule_hash = {}
-        rule_list.each do |rule|
-          rule[:code]
-          rule_hash["rule#{rule[:code]}"] = rule
-        end
-
-        File.open(output_file, "w") do |file|
-          file.puts JSON.pretty_generate(rule_hash)
-        end
+      hash[:name] = row["name"]
+      hash[:method] = row["name"].gsub(" ", "_").gsub(">", "_").gsub("-", "_").gsub("/", "_").downcase
+      hash[:message] = row["message"]
+      #hash[:object] = "BioSample, BioProject and Submission AND Run".split(/and|,/i).map {|obj| obj.chomp.strip }
+      hash[:object] = row["object"].split(/and|,/i).map {|obj| obj.chomp.strip }
+      unless row["reference"].nil? || row["reference"].strip == ""
+        hash[:reference] = row["reference"]
       end
+      rule_config.push(hash)
+    end
 
-      # delete temp file
-      system(%Q[rm #{csv_file}])
+    # output to current dir
+    output_dir = File.absolute_path(File.dirname(__FILE__))
+    # separate the rule config file to each validator
+    rule_classes = rule_config.group_by {|rule| rule[:rule_class]}
+    rule_classes.each do |rule_class, rule_list|
+      output_file = "#{output_dir}/rule_config_#{rule_class.downcase}.json"
+      rule_hash = {}
+      rule_list.each do |rule|
+        rule[:code]
+        rule_hash["rule#{rule[:code]}"] = rule
+      end
+      File.open(output_file, "w") do |file|
+        file.puts JSON.pretty_generate(rule_hash)
+      end
     end
   end
 end
 
-if ARGV.size == 0
-  puts "Usage: ruby rule2json.rb <biosample | bioproject | dra>"
+if ARGV.size < 2
+  puts "Usage: ruby rule2json.rb <biosample | bioproject | dra> rule_file.csv"
   exit(1);
 end
 creator = CreateRuleJson.new
-creator.create_rule_file(ARGV[0])
+creator.create_rule_file(ARGV[0], ARGV[1])
